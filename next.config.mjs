51f7758d873602963515ac80f324ff85dde13577
Rizki Maulana <rrizki402@gmail.com>
2025-07-01 23:52:02 +0700

    fix rute app

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,20 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   reactStrictMode: true,
-// };
-
-// export default nextConfig;
-
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    domains: [
-      "firebasestorage.googleapis.com",
-      "images.unsplash.com"
-    ],
-  },
 };
 
 export default nextConfig;
+
+
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: true,
+//   images: {
+//     domains: [
+//       "firebasestorage.googleapis.com",
+//       "images.unsplash.com"
+//     ],
+//   },
+// };
+
+// export default nextConfig;
